1bd9d06f377b75c98dbaa6b9a89b0e88f6f7df4d
Iago Barbosa <47748016+iago-barbosa@users.noreply.github.com>
2024-11-04 20:45:53 -0300

    criação do header mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,43 @@
-import { List } from 'react-bootstrap-icons';
+import { List, X, House, PCircle, Person } from 'react-bootstrap-icons';
 import './Header.scss';
 import {ReactComponent as BorderHeader } from './../../assets/border-header.svg';
+import logo from './../../assets/Logo-Passo-a-Passo.png';
+import React from 'react';
 
 export const Header = () => {
+    const [isOpenMenu, setIsOpenMenu] = React.useState(false);
 
     return(
         <header>
             <div className='header-container'>
-                <button className='menu btn' >
-                    <List/>
+                <button className='menu-btn btn' >
+                    <List onClick={() => setIsOpenMenu(!isOpenMenu)}/>
                 </button>
+                <div className={`menu ${!isOpenMenu ? 'close' : 'open'}`}>
+                    <div className='menu-header'>
+                        <X onClick={() => setIsOpenMenu(!isOpenMenu)}/>
+                    </div>
+                    <div className='logo-container'>
+                        <span>Clinica</span>
+                        <img className='logo' src={logo} alt="Logo" />
+                        <span>Passo a Passo</span>
+                    </div>
+                    <div className='menu-items'>
+                        <div className='item'>
+                            <House />
+                            <h3>Home</h3>
+                        </div>
+                        <div className='item'>
+                            <PCircle />
+                            <h3>Cadastrar</h3>
+                        </div>
+                        <div className='item'>
+                            <Person />
+                            <h3>Pacientes</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className={`menu-background ${!isOpenMenu ? 'close' : ''}`} onClick={() => setIsOpenMenu(!isOpenMenu)}></div>
             </div>
             <BorderHeader className='border-header'/>
         </header>
