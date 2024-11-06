2ef1f74019a2b16d851eaaa4504389246cb32024
Iago Barbosa <47748016+iago-barbosa@users.noreply.github.com>
2024-11-06 05:24:01 -0300

    adicionando configurações para o vercel

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: "http://localhost:3300"
-    baseURL: "http://3.140.193.152:3300"
+    baseURL: "/api"
 });
 
 export default api;
