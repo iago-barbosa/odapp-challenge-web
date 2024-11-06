03d0f5a071038eaf36d5d67de2631654dc4e2da9
Iago Barbosa <47748016+iago-barbosa@users.noreply.github.com>
2024-11-06 04:54:33 -0300

    alterado para usar a api no servidor aws

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3300"
+    //baseURL: "http://localhost:3300"
+    baseURL: "http://3.140.193.152:3300"
 });
 
 export default api;
