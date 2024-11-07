68b3eead3a006359a55e11adbf679bdffd81a34b
Iago Barbosa <47748016+iago-barbosa@users.noreply.github.com>
2024-11-07 04:12:31 -0300

    alterado a rota da api para pegar o ambiente de produção no vercel

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "3.140.193.152:3300"
-    //baseURL: "/api"
+    // servidor aws
+    //baseURL: "3.140.193.152:3300"
+    // vercel
+    baseURL: "https://odapp-challenge-backend.vercel.app"
 });
 
 export default api;
